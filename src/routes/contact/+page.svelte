4e9d9fce7f40e9e1e1433f2a6852df2b092209cd
Tristan Panco <tristan@Tristans-MacBook-Pro.local>
2022-12-04 12:23:08 +0200

    form

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,16 +1,56 @@
 <script>
   $: revealNumber = false;
   $: revealEmail = false;
+  let name = "";
+  let businessEmail = "";
+  let message = "";
+  let sent = false;
+
+  const sendMessage = (nameF, businessEmailF, messageF) => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      "data": {
+        "Name": nameF,
+        "BusinessEmail": businessEmailF,
+        "Message": messageF
+      }
+    });
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+
+    fetch("https://jellyfish-app-9zisi.ondigitalocean.app/api/contact-requests", requestOptions)
+      .then(response => response.text())
+      .then(result => {
+        sent = true;
+        setTimeout(() => {
+          sent = false;
+          name = "";
+          businessEmail = "";
+          message = "";
+        }, 1500);
+      })
+      .catch(error => console.log('error', error));
+    }
 </script>
 
+<div class="pop-up-thanks" style="transform: {sent ? "scale(1) translateX(-50%)" : "scale(0)"}">
+  <span>Thank You!<br>We will soon contact you</span>
+</div>
 <div class="contact">
   <div class="container">
     <h1>Contact</h1>
     <div class="contact-form">
-      <input type="text" placeholder="Name">
-      <input type="email" placeholder="Business Email" id="email">
-      <textarea id="message" placeholder="Message" cols="30" rows="3"></textarea>
-      <button>Send</button>
+      <input type="text" placeholder="Name" bind:value={name}>
+      <input type="email" placeholder="Business Email" id="email" bind:value={businessEmail}>
+      <textarea id="message" placeholder="Message" cols="30" rows="3" bind:value={message}></textarea>
+      <button on:click={() => sendMessage(name, businessEmail, message)}>Send</button>
     </div>
     <div class="contact-now">
       <div class="reveal"
@@ -30,6 +70,21 @@
 </div>
 
 <style lang=scss>
+  .pop-up-thanks {
+    position: absolute;
+    padding: 20px;
+    border-radius: 15px;
+    box-shadow: 0 0 15px rgba(0, 0, 0, .25);
+    font-size: 1.5rem;
+    font-weight: 600;
+    background-color: white;
+    top: 30%;
+    left: 50%;
+    transform-origin: center !important;
+    transform: translateX(-50%);
+    text-align: center;
+  }
+
   .contact {
     h1 {
       font-size: 250%;
